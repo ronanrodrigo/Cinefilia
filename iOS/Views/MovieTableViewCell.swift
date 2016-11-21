cbe63a8177ccea97cb409cf019452804606c99fb
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-11-21 16:38:31 -0200

    Updated movie table view cell

--- iOS/Views/MovieTableViewCell.swift
+++ iOS/Views/MovieTableViewCell.swift
@@ -2,4 +2,9 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var backdrop: UIImageView!
+    @IBOutlet weak var genresImages: UIStackView!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var title: UILabel!
+
 }
