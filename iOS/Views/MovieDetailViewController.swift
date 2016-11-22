c5586a116519166fd00ea56680434e253e347aca
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-11-22 14:26:02 -0200

    Created movie detail view controller

---
+++ iOS/Views/MovieDetailViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+import Shared
+
+class MovieDetailViewController: UIViewController {
+
+    @IBOutlet weak var movieBackdrop: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieOverview: UILabel!
+    @IBOutlet weak var movieReleaseDate: UILabel!
+    @IBOutlet weak var movieGenresImages: UIStackView!
+
+    private var movie: Movie?
+    private var movieGenres: [Genre]?
+    private var movieBackdropImage: UIImage?
+
+    convenience init(movie: Movie, movieGenres: [Genre], movieBackdropImage: UIImage?) {
+        self.init()
+        self.movie = movie
+        self.movieGenres = movieGenres
+        self.movieBackdropImage = movieBackdropImage
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        configureGenres()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    private func configureView() {
+        guard let movie = movie else { return }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        movieReleaseDate.text = dateFormatter.string(from: movie.releaseDate)
+        movieBackdrop.image = movieBackdropImage
+        movieTitle.text = movie.title.uppercased()
+        movieOverview.text = movie.overview
+    }
+
+    private func configureGenres() {
+        guard let movieGenres = movieGenres else { return }
+        for genre in movieGenres {
+            let genreImage = UIImage(named: genre.name)
+            let genreImageView = UIImageView(image: genreImage)
+            genreImageView.contentMode = UIViewContentMode.scaleAspectFit
+            movieGenresImages.addArrangedSubview(genreImageView)
+        }
+    }
+
+}
